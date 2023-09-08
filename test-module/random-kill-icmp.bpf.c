6d11db20e25198e347559798d31ed2e8d4e83b9b
Patrick MARIE <pm@mkz.me>
2023-09-08 08:21:54 +0200

    Update random-kill-icmp

--- test-module/random-kill-icmp.bpf.c
+++ test-module/random-kill-icmp.bpf.c
@@ -11,12 +11,24 @@
 
 #define OVER(x, d) (x + 1 > (typeof(x))d)
 
-int counter = 0;
+static inline void csum_replace2(uint16_t *sum, uint16_t old, uint16_t new)
+{
+	uint16_t csum = ~*sum;
+
+	csum += ~old;
+	csum += csum < (uint16_t)~old;
+
+	csum += new;
+	csum += csum < (uint16_t)new;
+
+	*sum = ~csum;
+}
 
 SEC("xdp")
 int random_kill_icmp(struct xdp_md *ctx) {
 	void *data_end = (void *)(uintptr_t)ctx->data_end;
 	void *data = (void *)(uintptr_t)ctx->data;
+	uint8_t old_ttl;
 
 	struct ethhdr *eth = data;
 	struct iphdr *iph = (struct iphdr *)(eth + 1);
@@ -41,13 +53,27 @@ int random_kill_icmp(struct xdp_md *ctx) {
 		return XDP_DROP;
 
 	/* let other ICMP pass */
-	if (icmph->type != ICMP_ECHO)
+	if (icmph->type != ICMP_ECHO && icmph->type != ICMP_ECHOREPLY)
 		return XDP_PASS;
 
-    /* randomly kill some requests */
-    if(bpf_get_prandom_u32() % 4 == 0) {
-		bpf_printk("Dropped icmp echo seq: %d", ntohs(icmph->un.echo.sequence));
-		return XDP_DROP;
+	switch (icmph->type) {
+		case ICMP_ECHO:
+			/* randomly kill some requests */
+			if(bpf_get_prandom_u32() % 4 == 0) {
+				bpf_printk("Dropped icmp echo seq: %d", ntohs(icmph->un.echo.sequence));
+				return XDP_DROP;
+			}
+			break;
+		case ICMP_ECHOREPLY:
+			/* set the TTL to a pseudorandom number 1..255 */
+			old_ttl = iph->ttl;
+			iph->ttl = bpf_get_prandom_u32() & 0xff ?: 1;
+
+			/* recalculate the checksum, otherwise the IP stack wil drop it */
+			csum_replace2(&iph->check, htons(old_ttl << 8), htons(iph->ttl << 8));
+			bpf_printk("Applying new TTL: %d", iph->ttl);
+
+			break;
 	}
 
     return XDP_PASS;
